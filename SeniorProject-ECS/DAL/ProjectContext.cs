292b1bbf4a415759678f6e8ffd0de72e72f31476
Chris Kingdon <Chris Kingdon>
2017-09-27 14:07:30 -0400

    Setup DbInitializer for entity framework

---
+++ SeniorProject-ECS/DAL/ProjectContext.cs
@@ -0,0 +1,25 @@
+﻿using SeniorProject_ECS.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace SeniorProject_ECS.DAL
+{
+    public class ProjectContext : DbContext
+    {
+        public ProjectContext() : base("ProjectContext")
+        {
+        }
+
+        public DbSet<StaffMember> StaffMembers { get; set; }
+        public DbSet<Center> Centers { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
