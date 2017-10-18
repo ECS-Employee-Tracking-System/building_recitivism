d63af065afba7cab9a8f7c9d013ebb221ae876c2
ckchessmaster <Genesis1:1>
2017-10-18 10:41:57 -0400

    added utility class

--- SeniorProjectECS/Controllers/StaffMemberController.cs
+++ SeniorProjectECS/Controllers/StaffMemberController.cs
@@ -13,28 +13,28 @@ namespace SeniorProjectECS.Controllers
     {
         public IActionResult Index()
         {
-            //var con = DBHandler.GetSqlConnection();
+            var con = DBHandler.GetSqlConnection();
             // do a join to get the centers as well
-            //String sql = "SELECT * FROM StaffMember LEFT JOIN Center ON StaffMember.CenterID = Center.CenterID";
+            String sql = "SELECT * FROM StaffMember LEFT JOIN Center ON StaffMember.CenterID = Center.CenterID";
 
             // tell dapper that we want multiple objects mapped from our query (staff member and center)
-            //var staffMembers = con.Query<StaffMember, Center, StaffMember>(sql, (staffMember, center) => { staffMember.Center = center; return staffMember; }, splitOn: "CenterID");
+            var staffMembers = con.Query<StaffMember, Center, StaffMember>(sql, (staffMember, center) => { staffMember.Center = center; return staffMember; }, splitOn: "CenterID");
 
             // offline testing
-            Center testCenter = new Center();
-            testCenter.Name = "UNF";
-            testCenter.County = "Duval";
-            testCenter.Region = "North East Florida";
-            StaffMember testStaff = new StaffMember();
-            testStaff.FirstName = "Chris";
-            testStaff.LastName = "Kingdon";
-            testStaff.Email = "n00827188@ospreys.unf.edu";
-            testStaff.Position = "Developer";
-            testStaff.DateOfHire = new DateTime(2017, 8, 1);
-            testStaff.Center = testCenter;
+            //Center testCenter = new Center();
+            //testCenter.Name = "UNF";
+            //testCenter.County = "Duval";
+            //testCenter.Region = "North East Florida";
+            //StaffMember testStaff = new StaffMember();
+            //testStaff.FirstName = "Chris";
+            //testStaff.LastName = "Kingdon";
+            //testStaff.Email = "n00827188@ospreys.unf.edu";
+            //testStaff.Position = "Developer";
+            //testStaff.DateOfHire = new DateTime(2017, 8, 1);
+            //testStaff.Center = testCenter;
 
-            List<StaffMember> staffMembers = new List<StaffMember>();
-            staffMembers.Add(testStaff);
+            //List<StaffMember> staffMembers = new List<StaffMember>();
+            //staffMembers.Add(testStaff);
 
             return View(staffMembers);
         }
