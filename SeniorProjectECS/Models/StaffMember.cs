9c8d5931f472f371395ecc8d3b88b54b1284107b
Chris Kingdon <ckgiveout@gmail.com>
2017-11-15 18:18:47 -0500

    Finished implementing dapper handlers for get, on staff member

--- SeniorProjectECS/Models/StaffMember.cs
+++ SeniorProjectECS/Models/StaffMember.cs
@@ -19,7 +19,6 @@ namespace SeniorProjectECS.Models
         public String Email { get; set; }
         public DateTime DateOfHire { get; set; }
         public String Position { get; set; }
-        public int CenterID { get; set; }
         public Boolean DirectorCredentials { get; set; }
         public DateTime DCExpiration { get; set; }
         public Boolean CDAInProgress { get; set; }
@@ -40,85 +39,12 @@ namespace SeniorProjectECS.Models
         public String Notes{ get; set; }
         public String TermDate { get; set; }
 
-        public String CenterName { get; set; }
-        public String CenterCounty { get; set; }
-        public String CenterRegion { get; set; }
-
-        public String DegreeAbrv { get; set; }
-        public String DegreeLevel { get; set; }
-        public String DegreeType { get; set; }
-        public String DegreeDetail { get; set; }
-
         public Center Center { get; set; }
-        public IEnumerable<Education> Education { get; set; }
+        public List<Education> Education { get; set; }
 
-        /// <summary>
-        /// This function gets a staff member and build all object nessisary for it.
-        /// </summary>
-        /// <param name="id">The ID of the staff member that we want to get.</param>
-        /// <returns>A staff member</returns>
-        public static StaffMember GetStaffMember(int? id)
+        public StaffMember()
         {
-            if (id == null) { return null; }
-            var con = DBHandler.GetSqlConnection();
-
-            String sql = "SELECT * FROM StaffMember WHERE StaffMemberID=" + id;
-            var staffMember = con.Query<StaffMember>(sql).First();
-
-            BuildStaffMember(staffMember, con);
-
-            return staffMember;
-        }// end GetStaffMember
-
-        /// <summary>
-        /// This function gets all staff members that fall within certain criteria.
-        /// </summary>
-        /// <param name="where">The WHERE statement, leave blank to get everything</param>
-        /// <returns>An IEnumerable contianing a list of StaffMembers</returns>
-        public static IEnumerable<StaffMember> GetStaffMembers(String where)
-        {
-            var con = DBHandler.GetSqlConnection();
-            String sql;
-
-            // check if we want to use the WHERE statement
-            if (where.Length > 0)
-            {
-               sql = "SELECT * FROM StaffMember WHERE " + where;
-            } else {
-                sql = "SELECT * FROM StaffMember";
-            }//end length check
-            
-            var staffMembers = con.Query<StaffMember>(sql);
-
-            // add data from other tables
-            foreach(var staff in staffMembers)
-            {
-                BuildStaffMember(staff, con);
-            }//end data
-
-            return staffMembers;
-        }//end GetSTaffMembers
-
-        /// <summary>
-        /// Create the rest of a staff member.
-        /// </summary>
-        /// <param name="staff">The staff member to finish building</param>
-        /// <param name="con">The SQLConnection</param>
-        private static void BuildStaffMember(StaffMember staff, SqlConnection con)
-        {
-            // add the center
-            String sql = "SELECT * FROM Center WHERE CenterID=" + staff.CenterID;
-            var center = con.Query<Center>(sql).First();
-            staff.Center = center;
-
-            // add the education
-            sql = "SELECT * FROM Education WHERE StaffMemberID=" + staff.StaffMemberID;
-            staff.Education = con.Query<Education>(sql);
-            foreach (var edu in staff.Education)
-            {
-                edu.StaffMember = staff;
-            }
-        }//end BuildStaffMember
-        
+            Education = new List<Education>();
+        }
     }//end class StaffMember
 }//end namespace SeniorProjecECS
