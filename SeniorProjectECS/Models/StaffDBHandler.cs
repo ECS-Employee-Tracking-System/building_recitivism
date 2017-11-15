23433b8573cfc7143d2d820ea8d1db9873b97f4e
Jerrad Monagan <jerradmonagan@gmail.com>
2017-11-15 04:52:23 -0500

    Added more fields to staff member, created index1 to show more options, experimented with sp

---
+++ SeniorProjectECS/Models/StaffDBHandler.cs
@@ -0,0 +1,90 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using SeniorProjectECS.Library;
+using Dapper;
+using System.Linq;
+namespace SeniorProjectECS.Models
+{
+    public class StaffDBHandler
+    {
+        private SqlConnection con;
+        private void Connection()
+        {
+            var con = DBHandler.GetSqlConnection();
+        }
+        // ********** VIEW Movie DETAILS ********************
+        public List<StaffMember> GetStaffMember()
+        {
+            Connection();
+            List<StaffMember> Stafflist = new List<StaffMember>();
+
+            SqlCommand cmd = new SqlCommand("GetStaffMemberDetails", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            SqlDataAdapter sd = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+
+            con.Open();
+            sd.Fill(dt);
+            con.Close();
+
+            foreach (DataRow dr in dt.Rows)
+            {
+
+                Stafflist.Add(
+                    new StaffMember
+                    {
+                        FirstName = Convert.ToString(dr["FirstName"]),
+                        LastName = Convert.ToString(dr["LastName"]),
+                        Email = Convert.ToString(dr["Email"]),
+                        DateOfHire = Convert.ToDateTime(dr["DateOfHire"]),
+                        Position = Convert.ToString(dr["Position"]),
+                        CenterID = Convert.ToInt32(dr["CenterID"]),
+                        DirectorCredentials = Convert.ToBoolean(dr["DirectorCredentials"]),
+                        DCExpiration = Convert.ToDateTime(dr["DCExpiration"]),
+                        CDAInProgress = Convert.ToBoolean(dr["CDAInProgress"]),
+                        CDAType = Convert.ToString(dr["CDAType"]),
+                        CDAExpiration = Convert.ToDateTime(dr["CDAExpiration"]),
+                        CDARenewalProcess = Convert.ToString(dr["CDARenewalProcess"]),
+                        Comments = Convert.ToString(dr["Comments"]),
+                        Goal = Convert.ToString(dr["Goal"]),
+                        MidYear = Convert.ToString(dr["MidYear"]),
+                        EndYear = Convert.ToString(dr["EndYear"]),
+                        GoalMet = Convert.ToBoolean(dr["GoalMet"]),
+                        TAndAApp = Convert.ToString(dr["TAndAApp"]),
+                        AppApp = Convert.ToString(dr["AppApp"]),
+                        ClassCompleted = Convert.ToString(dr["ClassCompleted"]),
+                        ClassPaid = Convert.ToBoolean(dr["ClassPaid"]),
+                        RequiredHours = Convert.ToInt32(dr["RequiredHours"]),
+                        HoursEarned = Convert.ToInt32(dr["HoursEarned"]),
+                        Notes = Convert.ToString(dr["Notes"]),
+                        TermDate = Convert.ToString(dr["TermDate"]),
+
+                    });
+            }
+            return Stafflist;
+        }
+
+        public List<StaffMember> GetAllStaff()
+        {
+            try
+            {
+                Connection();
+                con.Open();
+                IList<StaffMember> StaffList = SqlMapper.Query<StaffMember>(
+                                  con, "GetStaffMemberDetails").ToList();
+                con.Close();
+                return StaffList.ToList();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+    }
+    
+}
--- SeniorProjectECS/Models/StaffMember.cs
+++ SeniorProjectECS/Models/StaffMember.cs
@@ -5,6 +5,8 @@ using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Configuration;
+
 
 namespace SeniorProjectECS.Models
 {
@@ -17,13 +19,28 @@ namespace SeniorProjectECS.Models
         public String Email { get; set; }
         public DateTime DateOfHire { get; set; }
         public String Position { get; set; }
-
         public int CenterID { get; set; }
-        public Center Center { get; set; }
-
+        public Boolean DirectorCredentials { get; set; }
+        public DateTime DCExpiration { get; set; }
+        public Boolean CDAInProgress { get; set; }
         public String CDAType { get; set; }
         public DateTime CDAExpiration { get; set; }
+        public String CDARenewalProcess { get; set; }
+        public String Comments { get; set; }
+        public String Goal { get; set; }
+        public String MidYear { get; set; }
+        public String EndYear { get; set; }
+        public Boolean GoalMet { get; set; }
+        public String TAndAApp { get; set; }
+        public String AppApp { get; set; }
+        public String ClassCompleted { get; set; }
+        public Boolean ClassPaid { get; set; }
+        public int RequiredHours { get; set; }
+        public int HoursEarned { get; set; }
+        public String Notes{ get; set; }
+        public String TermDate { get; set; }
 
+        public Center Center { get; set; }
         public IEnumerable<Education> Education { get; set; }
 
         /// <summary>
@@ -93,5 +110,6 @@ namespace SeniorProjectECS.Models
                 edu.StaffMember = staff;
             }
         }//end BuildStaffMember
+        
     }//end class StaffMember
 }//end namespace SeniorProjecECS
