041437fdbb8ad6f3b541d2588ba68adab217eb99
Chris Kingdon <ckgiveout@gmail.com>
2017-11-15 19:23:30 -0500

    fixed nulls

--- SeniorProjectECS/Models/StaffMember.cs
+++ SeniorProjectECS/Models/StaffMember.cs
@@ -17,23 +17,28 @@ namespace SeniorProjectECS.Models
         public String FirstName { get; set; }
         public String LastName { get; set; }
         public String Email { get; set; }
-        public DateTime DateOfHire { get; set; }
+        public DateTime? DateOfHire { get; set; }
         public String Position { get; set; }
+
         public Boolean DirectorCredentials { get; set; }
-        public DateTime DCExpiration { get; set; }
+        public DateTime? DCExpiration { get; set; }
+
         public Boolean CDAInProgress { get; set; }
         public String CDAType { get; set; }
-        public DateTime CDAExpiration { get; set; }
+        public DateTime? CDAExpiration { get; set; }
         public String CDARenewalProcess { get; set; }
+
         public String Comments { get; set; }
         public String Goal { get; set; }
         public String MidYear { get; set; }
         public String EndYear { get; set; }
         public Boolean GoalMet { get; set; }
+
         public String TAndAApp { get; set; }
         public String AppApp { get; set; }
         public String ClassCompleted { get; set; }
         public Boolean ClassPaid { get; set; }
+
         public int RequiredHours { get; set; }
         public int HoursEarned { get; set; }
         public String Notes{ get; set; }
