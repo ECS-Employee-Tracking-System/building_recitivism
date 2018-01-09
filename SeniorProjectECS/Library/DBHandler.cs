797c7d770ae96befcd3aa38546c88d4f1a87e722
ckchessmaster <Genesis1:1>
2018-01-09 14:14:52 -0500

    fixed readability issue with center/staff

--- SeniorProjectECS/Library/DBHandler.cs
+++ SeniorProjectECS/Library/DBHandler.cs
@@ -3,6 +3,7 @@ using SeniorProjectECS.Models;
 using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -18,10 +19,18 @@ namespace SeniorProjectECS.Library
         /// <returns>A SQLConnection to a database</returns>
         public static SqlConnection GetSqlConnection()
         {
-            String connectionString = "Your conn string here";
+            String connectionString = LoadConnectionString();
             SqlConnection connection = new SqlConnection(connectionString);
 
             return connection;
+        }
+
+        private static String LoadConnectionString()
+        {
+            using (StreamReader reader = File.OpenText("connectionString.txt"))
+            {
+                return reader.ReadLine();
+            }
         }
     }
 }
