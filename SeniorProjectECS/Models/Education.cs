f00ba22c8fe5b5090fcfc0f368636963803c287d
Jerrad Monagan <jerradmonagan@gmail.com>
2018-01-20 11:51:42 -0500

    Added more details to the display names for more understanding for the user.

--- SeniorProjectECS/Models/Education.cs
+++ SeniorProjectECS/Models/Education.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,9 +9,17 @@ namespace SeniorProjectECS.Models
     public class Education
     {
         public int EducationID { get; set; }
+
+        [DisplayName("Degree Abbreviation ex. AA")]
         public String DegreeAbrv { get; set; } // MS
+
+        [DisplayName("Degree Level ex. Masters")]
         public String DegreeLevel { get; set; } // Masters
+
+        [DisplayName("Degree Type ex. Education")]
         public String DegreeType { get; set; } // Computing
+         
+        [DisplayName("Degree Detail ex. Early Childhood")]
         public String DegreeDetail { get; set; } // Master of Sciences in Computer Science
     }
 }
