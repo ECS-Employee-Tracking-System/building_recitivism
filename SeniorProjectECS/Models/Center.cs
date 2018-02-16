419933ea3cbe430e78a2c156661cf5812c911290
Jerrad Monagan <jerradmonagan@gmail.com>
2018-02-16 07:37:04 -0500

    made center required field to add user

--- SeniorProjectECS/Models/Center.cs
+++ SeniorProjectECS/Models/Center.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Data.SqlClient;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace SeniorProjectECS.Models
 {
@@ -13,12 +14,15 @@ namespace SeniorProjectECS.Models
         public int CenterID { get; set; }
 
         [DisplayName("Center Name")]
+        [Required]
         public String Name { get; set; }
 
         [DisplayName("Center County")]
+        [Required]
         public String County { get; set; }
 
         [DisplayName("Center Region")]
+        [Required]
         public String Region { get; set; }
 
         public List<StaffMember> Staff { get; set; }
