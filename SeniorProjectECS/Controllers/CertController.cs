d124c2d0aac09f1aa38d5238dfcb498a3b36d78d
Chris Kingdon <ckgiveout@gmail.com>
2018-02-27 11:36:04 -0500

    completed basic CRUD for certifications

---
+++ SeniorProjectECS/Controllers/CertController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SeniorProjectECS.Library;
+using Dapper;
+using SeniorProjectECS.Models;
+
+namespace SeniorProjectECS.Controllers
+{
+    public class CertController : Controller
+    {
+        // GET: Cert
+        public ActionResult Index()
+        {
+            var handle = new CertHandlerDapper();
+
+            return View(handle.GetModels());
+        }
+
+        // GET: Cert/Details/5
+        public ActionResult Details(int id)
+        {
+            var handle = new CertHandlerDapper();
+
+            return View(handle.GetModel(id));
+        }
+
+        // GET: Cert/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Cert/Create
+        [HttpPost]
+        public ActionResult Create(Certification model)
+        {
+            var handle = new CertHandlerDapper();
+            handle.AddModel(model);
+
+            return RedirectToAction("Index");
+        }
+
+        // GET: Cert/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var handle = new CertHandlerDapper();
+
+            return View(handle.GetModel(id));
+        }
+
+        // POST: Cert/Edit/5
+        [HttpPost]
+        public ActionResult Edit(Certification model)
+        {
+            var handle = new CertHandlerDapper();
+            handle.UpdateModel(model);
+
+            return RedirectToAction("Index");
+        }
+
+        // GET: Cert/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var handle = new CertHandlerDapper();
+            handle.DeleteModel(id);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ SeniorProjectECS/Models/CertHandlerDapper.cs
@@ -0,0 +1,59 @@
+﻿using Dapper;
+using SeniorProjectECS.Library;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SeniorProjectECS.Models
+{
+    public class CertHandlerDapper : IModelHandler<Certification>
+    {
+        public void AddModel(Certification Model)
+        {
+            using (var con = DBHandler.GetSqlConnection())
+            {
+                String sql = "INSERT INTO Certification (CertName, CertExpireAmount) VALUES (@CertName, @CertExpire)";
+                con.Execute(sql, new { CertName = Model.CertName, CertExpire = Model.CertExpireAmount });
+            }
+        }
+
+        public void DeleteModel(int id)
+        {
+            using (var con = DBHandler.GetSqlConnection())
+            {
+                String sql = "DELETE FROM Certification WHERE CertificationID=@CertID";
+                con.Execute(sql, new { CertID = id });
+            }
+        }
+
+        public Certification GetModel(int id)
+        {
+            using (var con = DBHandler.GetSqlConnection())
+            {
+                var data = con.Query<Certification>("SELECT * FROM Certification WHERE CertificationID=@CertID", new { CertID = id });
+
+                return data.FirstOrDefault();
+            }
+        }
+
+        public IEnumerable<Certification> GetModels()
+        {
+            using (var con = DBHandler.GetSqlConnection())
+            {
+                var data = con.Query<Certification>("SELECT * FROM Certification");
+
+                return data;
+            }
+        }
+
+        public void UpdateModel(Certification Model)
+        {
+            using (var con = DBHandler.GetSqlConnection())
+            {
+                String sql = "UPDATE Certification SET CertName=@CertName, CertExpireAmount=@CertExpire WHERE CertificationID=@CertID";
+                con.Execute(sql, new { CertName = Model.CertName, CertExpire = Model.CertExpireAmount, CertID = Model.CertificationID });
+            }
+        }
+    }
+}
