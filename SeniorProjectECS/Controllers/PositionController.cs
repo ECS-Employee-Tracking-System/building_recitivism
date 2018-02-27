8090b1bc495f56ada59fb4b0d4f498eb905de57d
ckchessmaster <Genesis1:1>
2018-02-27 17:46:03 -0500

    completed add/remove for required certs on positions

--- SeniorProjectECS/Controllers/PositionController.cs
+++ SeniorProjectECS/Controllers/PositionController.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using SeniorProjectECS.Models;
+using SeniorProjectECS.Library;
+using Dapper;
 
 namespace SeniorProjectECS.Controllers
 {
@@ -58,6 +60,48 @@ namespace SeniorProjectECS.Controllers
             handle.UpdateModel(model);
 
             return RedirectToAction("Index");
+        }
+
+        /// <summary>
+        /// Remove a certificate from being required by a position.
+        /// </summary>
+        /// <param name="CertificationID">The certification to remove.</param>
+        /// <param name="PositionID">The position to modify.</param>
+        /// <returns></returns>
+        public IActionResult RemoveCertification(int? CertificationID, int? PositionID)
+        {
+            if(CertificationID != null && PositionID != null)
+            {
+                using (var con = DBHandler.GetSqlConnection())
+                {
+                    String sql = "DELETE FROM PositionReq WHERE PositionID=@PosID AND CertificationID=@CertID";
+                    con.Execute(sql, new { PosID = PositionID, CertID = CertificationID });
+                }
+            }
+
+            return RedirectToAction("Edit", new { id = PositionID.GetValueOrDefault() });
+        }
+
+        /// <summary>
+        /// Add a new required certificate to the position.
+        /// </summary>
+        /// <param name="PositionID">The ID of the position to change.</param>
+        /// <param name="CertificationID">The ID of the certification to add.</param>
+        /// <returns></returns>
+        public ActionResult AddRequiredCert(int? PositionID, int? CertificationID)
+        {
+            if(PositionID != null && CertificationID != null)
+            {
+                using(var con = DBHandler.GetSqlConnection())
+                {
+                    String sql = "INSERT INTO PositionReq (PositionID, CertificationID) VALUES (@PosID, @CertID)";
+                    try {
+                        con.Execute(sql, new { PosID = PositionID.GetValueOrDefault(), CertID = CertificationID.GetValueOrDefault() });
+                    } catch (System.Data.SqlClient.SqlException e) { } 
+                }
+            }
+
+            return RedirectToAction("Edit", new { id = PositionID.GetValueOrDefault() });
         }
     }
 }
