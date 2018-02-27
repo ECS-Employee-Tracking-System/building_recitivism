bd5f81b3931c2fafedadd1c3caf5d35b3257e9c4
Chris Kingdon <ckgiveout@gmail.com>
2018-02-27 13:33:52 -0500

    working on dbreconfig stuff

--- SeniorProjectECS/Models/PositionHandlerDapper.cs
+++ SeniorProjectECS/Models/PositionHandlerDapper.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Dapper;
+using SeniorProjectECS.Library;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -19,12 +21,24 @@ namespace SeniorProjectECS.Models
 
         public Position GetModel(int id)
         {
-            throw new NotImplementedException();
+            using (var con = DBHandler.GetSqlConnection())
+            {
+                String sql = "SELECT * FROM Position as p" +
+                    "Left Outer Join PositionReq as pr on pr.PositionID=p.PositionID" +
+                    "Left Outer Join Certification as c on c.CertificationID=pr.CertificationID" +
+                    "WHERE PositionID=@PosID";
+
+                return null;
+            }
         }
 
         public IEnumerable<Position> GetModels()
         {
-            throw new NotImplementedException();
+            using(var con = DBHandler.GetSqlConnection())
+            {
+                var data = con.Query<Position>("SELECT * FROM Position");
+                return data;
+            }
         }
 
         public void UpdateModel(Position Model)
