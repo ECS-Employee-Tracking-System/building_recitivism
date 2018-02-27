c100b80d131cf17497702bf1b2178d8813ba65ba
Chris Kingdon <ckgiveout@gmail.com>
2018-02-27 11:36:04 -0500

    completed basic CRUD for certifications

---
+++ SeniorProjectECS/Models/PositionHandlerDapper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SeniorProjectECS.Models
+{
+    public class PositionHandlerDapper : IModelHandler<Position>
+    {
+        public void AddModel(Position Model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteModel(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Position GetModel(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Position> GetModels()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateModel(Position Model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
