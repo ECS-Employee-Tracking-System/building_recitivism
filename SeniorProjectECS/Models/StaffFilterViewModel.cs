bed8725114e4e199aa949e06ad84cee0246c7228
ckchessmaster <Genesis1:1>
2018-03-03 20:16:15 -0500

    completed new filter form

---
+++ SeniorProjectECS/Models/StaffFilterViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SeniorProjectECS.Models
+{
+    public class StaffFilterViewModel
+    {
+        public List<StaffMember> StaffMembers { get; set; }
+        public Filter Filter { get; set; }
+
+        public StaffFilterViewModel()
+        {
+            StaffMembers = new List<StaffMember>();
+        }
+    }
+}
