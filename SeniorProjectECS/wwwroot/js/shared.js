7091b45ba7ba0e35a5e9abf6f9cbdb29a3e5e792
Jerrad Monagan <jerradmonagan@gmail.com>
2018-03-20 13:58:41 -0400

    added comments for js

--- SeniorProjectECS/wwwroot/js/shared.js
+++ SeniorProjectECS/wwwroot/js/shared.js
@@ -1,4 +1,5 @@
-﻿    $(function () {
+﻿//used to cause default alert for deleteValidate class items
+$(function () {
         $('.deleteValidate').click(function () {
             return window.confirm("You are about to delete this item. Are you sure?");
         });
