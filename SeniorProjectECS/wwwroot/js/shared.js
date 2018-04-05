0489afd194503bbca1c6ab4ab01530ed738abb08
Jerrad Monagan <jerradmonagan@gmail.com>
2018-04-05 14:23:59 -0400

    added all certs to diplay perspective expire dates on list

--- SeniorProjectECS/wwwroot/js/shared.js
+++ SeniorProjectECS/wwwroot/js/shared.js
@@ -3,4 +3,12 @@ $(function () {
         $('.deleteValidate').click(function () {
             return window.confirm("You are about to delete this item. Are you sure?");
         });
-    });
+});
+function findAndReplace(string, target, replacement) {
+
+    var i = 0, length = string.length;
+    for (i; i < length; i++) {
+        string = string.replace(target, replacement);
+    }
+    return string;
+}
